0efc69ea8ec906f08196b31fced38cc36efd1ffd
Lucas Castro <lucas@uniforge.com>
2015-06-15 01:37:37 -0400

    Fixed 404 errors.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var httpServer = require('http-server');
 
 var server = httpServer.createServer({
-	root:'~/',
-	robots: true,
-	headers: {
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Credentials': 'true'
-	}
+   robots: true,
+   headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true'
+   }
 }).listen(process.env.PORT);
 // this.callback(null, server);
 console.log('Server running at ' + process.env.PORT);
